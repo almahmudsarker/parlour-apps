2c5f208e05588e26b76a436ca37a3c229ca59949
almahmudsarker <almahmuds427@gmail.com>
2023-10-07 22:30:49 +0600

    modify

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -8,7 +8,7 @@ export default function ErrorPage() {
     <div className="flex flex-col p-20 justify-center items-center" id="error-page">
       <img src="https://github.com/almahmudsarker/parlour-apps/blob/main/src/assets/images/Sorry,%20This%20page%20is%20not%20found..png?raw=true" alt="error" />
       <img src="https://github.com/almahmudsarker/parlour-apps/blob/main/src/assets/images/Frame.png?raw=true" alt="404" />
-      <Link to="/"><button className="w-[300px] h-[64px] rounded-xl text-white bg-[#F63E7B]">Back to Home</button></Link>
+      <Link to="/"><button className="w-[300px] h-[64px] rounded-xl text-white bg-[#F63E7B] mt-4">Back to Home</button></Link>
     </div>
   );
 }
