23269d8dd2dbcc0f8ae27de9d1b438f5156c08a0
almahmudsarker <almahmuds427@gmail.com>
2023-10-28 15:10:58 +0600

    centering components done

--- src/components/advertise/Ad.jsx
+++ src/components/advertise/Ad.jsx
@@ -1,42 +1,66 @@
-import React from 'react';
-import Container from '../shared/Container';
+import React from "react";
+import Container from "../shared/Container";
 
 const Ad = () => {
-    return (
-        <div className='w-full p-5'>
-        <Container>
-            <div className='flex flex-row py-8 gap-20'>
-            <div data-aos="fade-in"
-                data-aos-offset="200"
-                data-aos-delay="50"
-                data-aos-duration="1000"
-                data-aos-easing="ease-in-out"
-                data-aos-once="false">
-            <img className='w-[580px] h-[381px]' src='https://github.com/abirhasan33/jerin-parlour-client/blob/main/src/Assests/Images/Image/washing.png?raw=true' alt='728x90' />
-            </div>
-            <div data-aos="fade-up"
-                data-aos-offset="200"
-                data-aos-delay="50"
-                data-aos-duration="1000"
-                data-aos-easing="ease-in-out"
-                data-aos-once="false">
+  return (
+    <div className="w-full p-5 flex items-center justify-center">
+      <Container>
+        <div className="flex flex-row py-8 gap-20">
+          <div
+            data-aos="fade-in"
+            data-aos-offset="200"
+            data-aos-delay="50"
+            data-aos-duration="1000"
+            data-aos-easing="ease-in-out"
+            data-aos-once="false"
+          >
+            <img
+              className="w-[580px] h-[381px]"
+              src="https://github.com/abirhasan33/jerin-parlour-client/blob/main/src/Assests/Images/Image/washing.png?raw=true"
+              alt="728x90"
+            />
+          </div>
+          <div
+            data-aos="fade-up"
+            data-aos-offset="200"
+            data-aos-delay="50"
+            data-aos-duration="1000"
+            data-aos-easing="ease-in-out"
+            data-aos-once="false"
+          >
             <div>
-                <h1 className="text-4xl font-bold text-left text-[#111430] w-[409px] h-[82px] my-3">Let us handle your screen <span className='text-[#F63E7B]'>Professionally.</span></h1>
-                <p className="text-left text-[#666666] w-[401px] h-[98px]">With well written codes, we build amazing apps for all platforms, mobile and web apps in general ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus commodo ipsum.</p>
+              <h1 className="text-4xl font-bold text-left text-[#111430] w-[409px] h-[82px] my-3">
+                Let us handle your screen{" "}
+                <span className="text-[#F63E7B]">Professionally.</span>
+              </h1>
+              <p className="text-left text-[#666666] w-[401px] h-[98px]">
+                With well written codes, we build amazing apps for all
+                platforms, mobile and web apps in general ipsum. Lorem ipsum
+                dolor sit amet, consectetur adipiscing elit. Purus commodo
+                ipsum.
+              </p>
             </div>
-            <div className='flex mt-10'>
-              <p className="text-4xl font-bold text-left text-[#F63E7B] w-[110px] h-[60px] mt-5 mr-10">500+</p>
-              <p className="text-4xl font-bold text-left text-[#F63E7B] w-[70px] h-[60px] mt-5">16+</p>
+            <div className="flex mt-10">
+              <p className="text-4xl font-bold text-left text-[#F63E7B] w-[110px] h-[60px] mt-5 mr-10">
+                500+
+              </p>
+              <p className="text-4xl font-bold text-left text-[#F63E7B] w-[70px] h-[60px] mt-5">
+                16+
+              </p>
             </div>
-            <div className='flex'>
-              <span className="text-left text-[#666666] w-[140px] h-[24px]">Happy Customer</span>
-              <span className="text-left text-[#666666] w-[140px] h-[24px] ml-3">Total Services</span>
+            <div className="flex">
+              <span className="text-left text-[#666666] w-[140px] h-[24px]">
+                Happy Customer
+              </span>
+              <span className="text-left text-[#666666] w-[140px] h-[24px] ml-3">
+                Total Services
+              </span>
             </div>
-            </div>
-            </div>
-        </Container>
+          </div>
         </div>
-    );
+      </Container>
+    </div>
+  );
 };
 
 export default Ad;
--- src/components/banner/HeadBanner.jsx
+++ src/components/banner/HeadBanner.jsx
@@ -1,34 +1,53 @@
-import React from 'react';
-import Container from '../shared/Container';
+import React from "react";
+import Container from "../shared/Container";
 
 const HeadBanner = () => {
-    return (
-        <Container>
-            <div className="flex flex-row gap-5">
-              <div data-aos="fade-up"
-                data-aos-offset="200"
-                data-aos-delay="50"
-                data-aos-duration="1000"
-                data-aos-easing="ease-in-out"
-                data-aos-once="false">
-                <div className="flex flex-col gap-4 p-24">
-                    <h1 className="text-5xl font-bold text-left text-[#111430]">BEAUTY SALON</h1>
-                    <h1 className="text-5xl font-bold text-left text-[#111430]">FOR EVERY <span className='text-[#F63E7B]'>WOMEN</span></h1>
-                    <p className="text-left text-[#666666] w-[350px] h-[83px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus commodo ipsum duis laoreet maecenas. Feugiat</p>
-                    <button className="bg-[#F63E7B] text-white px-10 py-2 rounded-md w-[229px] h-[50]">Get an Appointment</button>
-                </div>
-                </div>
-                <div data-aos="fade-in"
-                data-aos-offset="200"
-                data-aos-delay="50"
-                data-aos-duration="1500"
-                data-aos-easing="ease-in-out"
-                data-aos-once="false">
-                <img src="https://github.com/abirhasan33/jerin-parlour-client/blob/main/src/Assests/Images/Image/header.png?raw=true" alt="banner" className="w-[484px] h-[478px] rounded-xl" /> 
-                </div>
+  return (
+    <Container>
+      <section className="items-center justify-center flex">
+        <div className="flex flex-row gap-5">
+          <div
+            data-aos="fade-up"
+            data-aos-offset="200"
+            data-aos-delay="50"
+            data-aos-duration="1000"
+            data-aos-easing="ease-in-out"
+            data-aos-once="false"
+          >
+            <div className="flex flex-col gap-4 p-24">
+              <h1 className="text-5xl font-bold text-left text-[#111430]">
+                BEAUTY SALON
+              </h1>
+              <h1 className="text-5xl font-bold text-left text-[#111430]">
+                FOR EVERY <span className="text-[#F63E7B]">WOMEN</span>
+              </h1>
+              <p className="text-left text-[#666666] w-[350px] h-[83px]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus
+                commodo ipsum duis laoreet maecenas. Feugiat
+              </p>
+              <button className="bg-[#F63E7B] text-white px-10 py-2 rounded-md w-[229px] h-[50]">
+                Get an Appointment
+              </button>
             </div>
-        </Container>
-    );
+          </div>
+          <div
+            data-aos="fade-in"
+            data-aos-offset="200"
+            data-aos-delay="50"
+            data-aos-duration="1500"
+            data-aos-easing="ease-in-out"
+            data-aos-once="false"
+          >
+            <img
+              src="https://github.com/abirhasan33/jerin-parlour-client/blob/main/src/Assests/Images/Image/header.png?raw=true"
+              alt="banner"
+              className="w-[484px] h-[478px] rounded-xl"
+            />
+          </div>
+        </div>
+      </section>
+    </Container>
+  );
 };
 
 export default HeadBanner;
