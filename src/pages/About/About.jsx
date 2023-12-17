9dde927daa1c1aa7a8444e966941758457332071
almahmudsarker <almahmuds427@gmail.com>
2023-12-18 01:43:44 +0600

    about done

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,12 +1,59 @@
-import React from "react";
 import Navbar from "../../components/shared/navbar/Navbar";
 import Footer from "../../components/shared/Footer/Footer";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const About = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // values from 0 to 3000, with step 50ms
+      easing: "ease", // default easing for AOS animations
+      once: false, // whether animation should happen only once - while scrolling down
+    });
+  }, []);
   return (
     <>
       <Navbar />
-      <h1 className="text-4xl text-center">About</h1>
+      <div className="bg-white text-gray-600 work-sans leading-normal text-base tracking-normal">
+        <section className="container mx-auto p-8">
+          <h1
+            className="text-4xl font-bold text-center text-gray-800 mb-5"
+            data-aos="fade-up"
+          >
+            Who We Are
+          </h1>
+          <p
+            className="mb-8 leading-relaxed"
+            data-aos="fade-up"
+            data-aos-delay="100"
+          >
+            Welcome to [Salon Name], where beauty and well-being converge...
+          </p>
+
+          <h2
+            className="text-3xl font-bold text-gray-800 mb-3"
+            data-aos="fade-left"
+          >
+            Our Philosophy
+          </h2>
+          <p className="mb-8 leading-relaxed" data-aos="fade-right">
+            We believe in a holistic approach to beauty, blending the
+            artistry...
+          </p>
+
+          {/* ...other sections with AOS data attributes... */}
+
+          <div className="text-center" data-aos="zoom-in-up">
+            <a
+              href="/"
+              className="bg-[#f86e9c] text-white  py-2 px-4 rounded-full hover:bg-[#ff75a3c6] transition duration-300"
+            >
+              Book an Appointment
+            </a>
+          </div>
+        </section>
+      </div>
       <Footer />
     </>
   );
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -6,7 +6,55 @@ const Contact = () => {
   return (
     <>
       <Navbar />
-      <h1 className="text-4xl text-center">Contact</h1>
+      <div className="container mx-auto p-8">
+        <div className="flex flex-wrap justify-center text-center mb-8">
+          <div className="w-full lg:w-6/12 px-4">
+            <h2 className="text-4xl font-semibold">Get in Touch</h2>
+            <p className="text-lg leading-relaxed m-4 text-gray-600">
+              Have questions, concerns, or just want to say hello? Feel free to
+              reach out to us.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-wrap">
+          <div className="w-full md:w-6/12 lg:w-4/12 px-4">
+            <div className="py-6">
+              <div className="mb-4">
+                <h3 className="text-2xl font-semibold">Visit Us</h3>
+              </div>
+              <p className="text-lg text-gray-600">
+                123 Main Street
+                <br />
+                Cityville, State 12345
+              </p>
+            </div>
+          </div>
+          <div className="w-full md:w-6/12 lg:w-4/12 px-4">
+            <div className="py-6">
+              <div className="mb-4">
+                <h3 className="text-2xl font-semibold">Contact Info</h3>
+              </div>
+              <p className="text-lg text-gray-600">
+                Email: info@example.com
+                <br />
+                Phone: +1 (555) 123-4567
+              </p>
+            </div>
+          </div>
+          <div className="w-full md:w-6/12 lg:w-4/12 px-4">
+            <div className="py-6">
+              <div className="mb-4">
+                <h3 className="text-2xl font-semibold">Social Media</h3>
+              </div>
+              <p className="text-lg text-gray-600">
+                Follow us on Twitter, Facebook, and Instagram for updates and
+                news.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
